c9453db07864f6750d8357450922de91fda4f218
Amrit <amritkarma@outlook.com>
2026-05-24 03:58:08 +0530

    add base path vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react-vite-portfolio/",
   plugins: [react(), tailwindcss()],
 });
